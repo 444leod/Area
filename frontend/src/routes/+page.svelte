ce1b3c737b0aff97407a19a1c85a84a7d77d0658
Léo DUMONT <114572827+leodumont444@users.noreply.github.com>
2024-09-19 15:04:04 +0200

    feat(AREA-1): added cluster config & frontend config & ci/cd for production (#3)
    Co-authored-by: jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Let's get cracking bones!</h1>
+		<p>hello ca marche j'adore la vie</p>
+		<h1 class="h1">
+			Bienvenu sur notre AREA !!!
+		</h1>
+	</div>
+</div>
