ad3a6821f886a41d9f5106550ba2a17ed3a73050
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-09-19 12:42:44 +0200

    fix(CI): using Kustomize now

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,7 +3,7 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
+		<p>hello ca marche j'adore la vie</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
 			<li><code class="code">/src/app.postcss</code> - app wide css</li>
