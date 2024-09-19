bd84a997540021792e9b6da5c9f8a2f456958f47
Léo DUMONT <leo.dumont@epitech.eu>
2024-09-20 00:08:22 +0200

    feat(AREA-24): added base POC worker architecture with database logic.

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,11 +1,56 @@
 // In frontend or worker
-import { User } from '@area/types';
+import {reactionsMap} from "./reactions/reactionsMap";
 
-const user: User = {
-    id: '1',
-    name: 'John Doe',
-    email: 'john@example.com',
-    password: 'password'
-};
+const { MongoClient, Db } = require('mongodb');
+require('dotenv').config();
 
-console.log(user);
+async function run(database: typeof Db) {
+    try {
+      const reactionsCollection = database.collection("reactions");
+
+      const reactions = await reactionsCollection.find({}).toArray();
+
+      for (const reaction of reactions) {
+        const func = reactionsMap[reaction.type || ""];
+        if (func) {
+          await func(reaction);
+        } else {
+          console.error(`type ${reaction.type} not found`);
+        }
+      }
+    } catch (err) {
+      console.error('Error in run function:', err);
+    }
+}
+
+async function main() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error('MONGODB_URI must be defined in .env');
+  }
+
+  const client = new MongoClient(uri);
+
+  try {
+    // Connexion à MongoDB
+    await client.connect();
+    const database = client.db('local');
+
+    await run(database);
+    setInterval(
+        async () => {
+          try {
+            await run(database);
+          } catch (err) {
+            console.error('Error in interval run:', err);
+          }
+        },
+        60000 // 1 minute
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main().catch(console.error);
