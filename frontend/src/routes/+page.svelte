e4d8dbe5527217a31ea01619bea113a37a191d7c
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-09-20 15:20:10 +0200

    fix(Database): not working with dokcer anymore, using helm charts is mush easier; moving on AREA-32 to work on mono repo thing

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,15 @@
-<script lang="ts">
-	import type { User } from "@area/types";
-	const user: User = {
-		id: "1",
-		name: "zljvnzv",
-		email: "zjvzjv",
-		password: "zjvzjv",
-	};
-	console.log(user);
-</script>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>hello ca marche j'adore la vie</p>
-		<h1 class="h1">
-			Bienvenu sur notre AREA !!!
-		</h1>
-		<p>
-			{JSON.stringify(user)}
-		</p>
+		<p>Start by exploring:</p>
+		<ul>
+			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
+			<li><code class="code">/src/app.postcss</code> - app wide css</li>
+			<li>
+				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+			</li>
+		</ul>
 	</div>
 </div>
