8d311095da8e77090b5d30d8ae25f550f9c5dcd4
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-09-23 18:38:54 +0200

    fix(api): imports

--- api/src/modules/webhooks.module.ts
+++ api/src/modules/webhooks.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
-import { AreasController } from "src/controllers/areas.controller";
-import { AreasService } from "src/services/areas.service";
+import { AreasController } from "@api/controllers/areas.controller";
+import { AreasService } from "@api/services/areas.service";
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
