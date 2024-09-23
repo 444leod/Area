49b0b870d7f8b8ee85252412c60555f6d8c1af9f
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-09-24 00:00:48 +0200

    refactor(areas): fit to new db scheme

--- api/src/controllers/areas.controller.ts
+++ api/src/controllers/areas.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { SendEmailDTO } from "@shared/dto/send_mail.dto";
-import { SendEmailResponse } from "@shared/dto/send_email.response";
-import { AreasService } from "@api/services/areas.service";
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendEmailDTO } from '@shared/dto/send_mail.dto';
+import { SendEmailResponse } from '@shared/dto/send_email.response';
+import { AreasService } from '@api/services/areas.service';
 
 @Controller('/areas')
 export class AreasController {
-
   constructor(private readonly areasService: AreasService) {}
 
   @Post()
-  async createArea(@Body() createAreaDto: SendEmailDTO) : Promise<SendEmailResponse> { // Change type to create area dto
+  async createEmailArea(
+    @Body() createAreaDto: SendEmailDTO,
+  ): Promise<SendEmailResponse> {
     return {
-      webhook_id: await this.areasService.createArea(createAreaDto).toString()
-    }
+      webhook_id: (await this.areasService.createArea(createAreaDto)).toString(),
+    };
   }
 }
--- api/src/services/areas.service.ts
+++ api/src/services/areas.service.ts
@@ -1,13 +1,12 @@
-import { Injectable } from "@nestjs/common";
-import { Db, InsertOneResult, MongoClient, ObjectId } from "mongodb";
-import { SendEmailDTO } from '@shared/dto/send_mail.dto'
+import { Injectable } from '@nestjs/common';
+import { Db, InsertOneResult, MongoClient, ObjectId } from 'mongodb';
+import { SendEmailDTO } from '@shared/dto/send_mail.dto';
 
 @Injectable()
 export class AreasService {
+  private db: Db;
 
-  private db : Db;
-
-  constructor () {
+  constructor() {
     const base_db_uri = process.env.DB_URI || '';
     console.log('DB URI = ' + base_db_uri);
     const client = new MongoClient(base_db_uri);
@@ -17,29 +16,25 @@ export class AreasService {
     });
   }
 
-  async createArea(dto: SendEmailDTO) : Promise<ObjectId> {
+  async createArea(dto: SendEmailDTO): Promise<ObjectId> {
     const areas = this.db.collection('areas');
-    const actions = this.db.collection('actions');
-    const reactions = this.db.collection('reactons');
+    const reactions = this.db.collection('reactions');
     const webhooks = this.db.collection('webhooks');
 
-    const result = await (async () => {
-      const actionResult = await actions.insertOne({});
-      const reactionResult = await reactions.insertOne(dto);
+    const hookResult = await webhooks.insertOne({
+      type: 'githubPullRequest',
+    });
 
-      console.log(actionResult);
-      console.log(reactionResult);
+    const reactionResult = await reactions.insertOne({
+      type: 'sendEmail',
+      data: dto,
+    });
 
-      const areaResult = await areas.insertOne({
-        action_id: actionResult.insertedId,
-        reaction_id: reactionResult.insertedId
-      });
-      return await webhooks.insertOne({
-        action_id: actionResult.insertedId,
-        reaction_id: reactionResult.insertedId,
-        area_id: areaResult.insertedId
-      });
-    })();
-    return result.insertedId;
+    const areaResult = await areas.insertOne({
+      webhook_id: hookResult.insertedId,
+      reaction_id: reactionResult.insertedId,
+    });
+
+    return hookResult.insertedId;
   }
 }
