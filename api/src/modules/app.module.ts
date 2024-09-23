e8d608fc21c22b0c3d89c4abef64e1e9bf28723b
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-09-23 23:59:44 +0200

    fix(app): removed imports from nknown files (dead)

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '../controllers/app.controller';
-import { AppService } from '../services/app.service';
 import { AreasModule } from './areas.module';
 import { WebhooksModule } from './webhooks.module';
 
 @Module({
   imports: [ AreasModule, WebhooksModule ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
