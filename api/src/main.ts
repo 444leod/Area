0e3a9d427588672e64ddc90d689e102249c3b2c0
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-09-23 14:48:16 +0200

    chore(api): moved components in subfolders

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,6 @@
+import { AppModule } from './modules/app.module';
+
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import {
   FastifyAdapter,
   NestFastifyApplication,
