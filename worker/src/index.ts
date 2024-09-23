064d57d2567eedd847dc6fa1c66fdb1365c39d15
Thibault Longuepee <114572782+ThibaultLonguepee@users.noreply.github.com>
2024-09-23 15:32:10 +0200

    ci(AREA-32): deployable shared folder (#8)
    Co-authored-by: jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
    Co-authored-by: Léo DUMONT <leo.dumont@epitech.eu>

---
+++ worker/src/index.ts
@@ -0,0 +1,11 @@
+// In frontend or worker
+import { User } from '@shared/user.dto';
+
+const user: User = {
+    id: '1',
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: 'password'
+};
+
+console.log(user);
