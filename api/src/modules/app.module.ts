927ac08ac63817fa78ad8cd7a77a8ea3e453c666
lemairetheo <114654759+lemairetheo@users.noreply.github.com>
2024-09-25 15:54:28 +0200

    feat(AREA-41) add route for login and register user (#13)

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AreasModule } from './areas.module';
 import { WebhooksModule } from './webhooks.module';
+import { authModule } from './auth.module';
 
 @Module({
-  imports: [ AreasModule, WebhooksModule ],
+  imports: [ AreasModule, WebhooksModule, authModule ],
   controllers: [],
   providers: [],
 })
