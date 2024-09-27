e48069ee2c1e9b86cfae0291b099dd6f7a79304e
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-09-27 12:53:08 +0200

    fix(AREA-42): fix based on comments

--- worker/src/exemple_worker.ts
+++ worker/src/exemple_worker.ts
@@ -1,16 +1,16 @@
 import { AreaDTO } from "@shared/dtos/area.dto";
-import { RabbitMQConnection } from "@shared/utils/RabbitMQConnection";
+import { RabbitMQService } from "@shared/utils/RabbitMQService";
 
-const connection = new RabbitMQConnection();
+const connection = new RabbitMQService();
 
-//Do mongodb connection here to not repeat the connection every time areaHandler is called
+//Do mongodb connection here to not repeat the connection every time handleArea is called
 
-function areaHandler(area: AreaDTO) {
+async function handleArea (area: AreaDTO) {
   // Put worker logic here
   console.log(area);
 }
 
 
 connection.connect().then(() => {
-  connection.consumeArea(areaHandler)
+  connection.consumeArea(handleArea)
 });
