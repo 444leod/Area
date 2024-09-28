9110271627fc191a0658f8912c7cd52bc0a42aad
AZAN Jean-Baptiste <114654392+jean-baptisteAZAN@users.noreply.github.com>
2024-09-28 15:24:35 +0200

    fix(AREA-61): fixed database connection from api (#20)

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,24 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from '../services/auth.service';
-import { User } from "@area/shared"
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "../services/auth.service";
+import { User } from "@area/shared";
+import { ApiTags } from "@nestjs/swagger";
 
-@ApiTags('Auth')
-@Controller('/auth')
+@ApiTags("Auth")
+@Controller("/auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/login')
+  @Post("/login")
   async login(@Body() loginDto: User) {
     const { email, password } = loginDto;
     return this.authService.login(email, password);
   }
 
-  @Post('/register')
+  @Post("/register")
   async register(@Body() loginDto: User) {
     const { email, password } = loginDto;
     const result = await this.authService.register(email, password);
 
-    return result
+    return result;
   }
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,27 +4,31 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
-import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from "@nestjs/swagger";
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
 
   const config = new DocumentBuilder()
     .setTitle("AREA API")
     .setDescription(
-      "The AREA API allows the creation of actions-reactions on multiple services; as well as account manager (login, registration)."
+      "The AREA API allows the creation of actions-reactions on multiple services; as well as account manager (login, registration).",
     )
     .setVersion("0.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  const options : SwaggerCustomOptions = {
+  const options: SwaggerCustomOptions = {
     swaggerOptions: {
-      defaultModelsExpandDepth: 5
-    }
-  }
+      defaultModelsExpandDepth: 5,
+    },
+  };
   SwaggerModule.setup("swagger", app, document, options);
 
   app.enableCors();
