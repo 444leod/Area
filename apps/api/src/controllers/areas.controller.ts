208fc1380978ed7bb00d26dc20546da0bc6586c1
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-09-28 14:48:54 +0200

    fix(api): db connection for areas auth and webhooks services

--- apps/api/src/controllers/areas.controller.ts
+++ apps/api/src/controllers/areas.controller.ts
@@ -3,15 +3,13 @@ import { SendEmailDTO } from "@area/shared";
 import { AreasService } from "../services/areas.service";
 import { ApiTags } from "@nestjs/swagger";
 
-@ApiTags('Areas')
+@ApiTags("Areas")
 @Controller("/areas")
 export class AreasController {
   constructor(private readonly areasService: AreasService) {}
 
   @Post()
-  async createEmailArea(
-    @Body() createAreaDto: SendEmailDTO
-  ): Promise<any> {
+  async createEmailArea(@Body() createAreaDto: SendEmailDTO): Promise<any> {
     return {
       webhook_id: (
         await this.areasService.createArea(createAreaDto)
--- apps/api/src/modules/app.module.ts
+++ apps/api/src/modules/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AreasModule } from './areas.module';
-import { WebhooksModule } from './webhooks.module';
-import { AuthModule } from './auth.module';
+import { Module } from "@nestjs/common";
+import { AreasModule } from "./areas.module";
+import { WebhooksModule } from "./webhooks.module";
+import { AuthModule } from "./auth.module";
 
 @Module({
-  imports: [ AuthModule, AreasModule, WebhooksModule ],
+  imports: [AuthModule, AreasModule, WebhooksModule],
   controllers: [],
   providers: [],
 })
