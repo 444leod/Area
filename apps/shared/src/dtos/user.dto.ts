9309e724c440ee87cc7f42f855b9d59dff2ee146
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-01 10:21:17 +0200

    fix(shared/User): disable versionKey on user schema

--- apps/shared/src/dtos/user.dto.ts
+++ apps/shared/src/dtos/user.dto.ts
@@ -2,18 +2,22 @@ import { Prop, Schema } from "@nestjs/mongoose";
 import { AreaDTO } from "./area.dto";
 import { ObjectId } from "mongodb";
 
-@Schema()
+@Schema({versionKey: false})
 export class User {
   _id: ObjectId;
 
   @Prop()
   first_name: string;
+
   @Prop()
   last_name: string;
+
   @Prop()
   email: string;
+
   @Prop()
   password: string;
+
   @Prop()
   areas: AreaDTO[];
 }
