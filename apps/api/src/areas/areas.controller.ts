703fc5717fb2aca943cb958481df9569741b3f30
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-02 14:07:56 +0200

    docs(/areas): added more swagger gen decorators for areas routes

--- apps/api/src/areas/areas.controller.ts
+++ apps/api/src/areas/areas.controller.ts
@@ -1,5 +1,19 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiHeader,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger";
 import { AuthGuard } from "../auth/auth.guard";
 import { Area, AreaCreationDto, AreaDto } from "@area/shared";
 import { UsersService } from "../users/users.service";
@@ -13,29 +27,94 @@ export class AreasController {
     private readonly usersService: UsersService
   ) {}
 
+  @ApiHeader({
+    name: "authorization",
+    description: "User API token, given at user login. (Bearer token)",
+  })
+  @ApiCreatedResponse({
+    description: "The AREA was successfully created.",
+    example: {
+      active: true,
+      action: {
+        service_id: "deadbeefdeadbeefdeadbeef",
+        informations: {
+          type: "EXAMPLE_TYPE",
+          field: "exampleFieldData",
+        },
+      },
+      reaction: {
+        service_id: "deadbeefdeadbeefdeadbeef",
+        informations: {
+          type: "EXAMPLE_TYPE",
+          field: "exampleFieldData",
+        },
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    description: "No token provided, or token isn't valid.",
+  })
   @Post()
   @UseGuards(AuthGuard)
-  async createArea(@Request() req, @Body() dto: AreaCreationDto) : Promise<Area> {
+  async createArea(
+    @Request() req,
+    @Body() dto: AreaCreationDto
+  ): Promise<Area> {
     const area = this.areasHelper.build(dto);
     this.usersService.addAreaToUser(req.user, area);
     return area;
   }
 
+  @ApiHeader({
+    name: "authorization",
+    description: "User API token, given at Log-In. (Bearer token)",
+  })
+  @ApiOkResponse({
+    description: "The data was successfully fetched.",
+    example: [
+      {
+        active: true,
+        action: {
+          service_id: "deadbeefdeadbeefdeadbeef",
+          informations: {
+            type: "EXAMPLE_TYPE",
+            field: "exampleFieldData",
+          },
+        },
+        reaction: {
+          service_id: "deadbeefdeadbeefdeadbeef",
+          informations: {
+            type: "EXAMPLE_TYPE",
+            field: "exampleFieldData",
+          },
+        },
+      },
+      {
+        active: false,
+        action: {},
+        reaction: {},
+      },
+    ],
+  })
+  @ApiUnauthorizedResponse({
+    description: "No token provided, or token isn't valid.",
+  })
   @Get()
   @UseGuards(AuthGuard)
-  async getUserAreas(@Request() req) : Promise<AreaDto[]> {
+  async getUserAreas(@Request() req): Promise<AreaDto[]> {
     const user = await this.usersService.findByEmail(req.user.email);
     return user.areas.map((area, _) => {
       return {
         active: area.active,
         action: {
           service_id: area.action.service_id,
-          informations: area.action.informations
+          informations: area.action.informations,
         },
         reaction: {
           service_id: area.reaction.service_id,
-          informations: area.reaction.informations
-        }
-      } as AreaDto});
+          informations: area.reaction.informations,
+        },
+      } as AreaDto;
+    });
   }
 }
