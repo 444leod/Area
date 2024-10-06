a8c4cd589e0c1b35b28b59a370763c91cd6c7f2c
Léo DUMONT <leo.dumont@epitech.eu>
2024-10-06 18:37:02 +0200

    hotfix(merge): fixed merge fail.

--- apps/api/src/auth/google.strategy.ts
+++ apps/api/src/auth/google.strategy.ts
@@ -21,22 +21,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         });
     }
 
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-    done: VerifyCallback,
-  ): Promise<any> {
-    const { name, emails, photos } = profile;
-    const user = {
-      email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
-      picture: photos[0].value,
-      accessToken,
-    };
-    done(null, user);
-  }
     async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
         const { name, emails, photos } = profile;
         const user = {
