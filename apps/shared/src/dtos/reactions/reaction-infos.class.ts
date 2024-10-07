4e919316e6c58332bf08684258cfb42a60c41f78
Léo DUMONT <114572827+leodumont444@users.noreply.github.com>
2024-10-07 09:50:40 +0200

    feat(AREA-96): added implementation for send email reaction (#50)

--- apps/shared/src/dtos/reactions/reaction-infos.class.ts
+++ apps/shared/src/dtos/reactions/reaction-infos.class.ts
@@ -1,6 +1,6 @@
 import { IsEnum } from "class-validator";
 import { ExampleReactionInfos } from "./example-reaction.class";
-import { SendEmailReactionInfos } from "./send-email.dto";
+import { SendEmailReactionInfos } from "./emails/send-email.dto";
 import { CreateGoogleTaskInfos } from "./google-tasks/create-google-task.class";
 import { ReactionTypes } from "./reaction-types.enum";
 
--- apps/worker/src/reactions/reactions-map.ts
+++ apps/worker/src/reactions/reactions-map.ts
@@ -1,6 +1,7 @@
 import { ReactionFunction } from './reaction-function';
 import { handleExampleReaction } from './example/example';
 import { handleCreateGoogleTaskReaction } from './google-tasks/create-google-task';
+import { handleSendEmailReaction } from './email/send-email';
 
 type ReactionMap = {
     [string: string]: ReactionFunction;
@@ -9,4 +10,5 @@ type ReactionMap = {
 export const reactionsMap: ReactionMap = {
     EXAMPLE_REACTION: handleExampleReaction,
     CREATE_GOOGLE_TASK: handleCreateGoogleTaskReaction,
+    SEND_EMAIL: handleSendEmailReaction,
 };
