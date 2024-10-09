5f78e248efbd1c789b90071978f0e6df63c71725
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-09 16:19:44 +0200

    refactor(AreasService): moved areas-related methods out of users service

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -24,36 +24,36 @@ export class UsersService {
   }): Promise<User> {
     let user = await this.userModel.findOne({ email: userData.email });
     if (user) {
-      const authIndex = user.user_authorization.findIndex(auth => 
-        auth.service_Id.equals(userData.service_Id) && auth.type === 'GOOGLE'
+      const authIndex = user.user_authorization.findIndex(
+        (auth) =>
+          auth.service_Id.equals(userData.service_Id) && auth.type === "GOOGLE",
       );
       if (authIndex !== -1) {
         user.user_authorization[authIndex].data = userData.token;
       } else {
         user.user_authorization.push({
           service_Id: userData.service_Id,
-          type: 'GOOGLE',
-          data: userData.token
+          type: "GOOGLE",
+          data: userData.token,
         });
       }
       return await user.save();
     }
-  
+
     const newUser = new this.userModel({
       first_name: userData.first_name,
       last_name: userData.last_name,
       email: userData.email,
-      user_authorization: [{
-        service_Id: userData.service_Id,
-        type: 'GOOGLE',
-        data: userData.token
-      }]
+      user_authorization: [
+        {
+          service_Id: userData.service_Id,
+          type: "GOOGLE",
+          data: userData.token,
+        },
+      ],
     });
     return await newUser.save();
   }
-  
-
-  
 
   async findById(id: string | ObjectId): Promise<User | undefined> {
     return await this.userModel.findById(id).exec();
@@ -61,44 +61,5 @@ export class UsersService {
 
   async findByEmail(email: string): Promise<User | undefined> {
     return await this.userModel.findOne({ email: email }).exec();
-  }
-
-  /** AREA RELATER */
-
-  async getUserArea(user: { sub: string }, id: ObjectId) : Promise<Area> {
-    const u = await this.userModel.findById(user.sub);
-    const area = u.areas.find(a => a._id.equals(id));
-    if (!area) throw new NotFoundException("Area Not Found.");
-    return area;
-  }
-
-  async addAreaToUser(user: { sub: string }, area: Area): Promise<User> {
-    const u = await this.userModel.findById(user.sub);
-    u.areas.push(area);
-    return await u.save();
-  }
-
-  async removeAreaFromUser(user: { sub: string }, id: ObjectId): Promise<User> {
-    const u = await this.userModel.findById(user.sub);
-    if (!u.areas.find((a) => a._id.equals(id)))
-      throw new NotFoundException("Area Not Found.");
-    u.areas = u.areas.filter((a) => !a._id.equals(id));
-    return await u.save();
-  }
-
-  async updateUserArea(user: { sub: string }, area: Area): Promise<Area> {
-    const u = await this.userModel.findById(user.sub);
-    const index = u.areas.findIndex(a => a._id.equals(area._id));
-    if (index < 0)
-      throw new NotFoundException("AREA not found");
-    u.areas[index] = area;
-    u.save();
-    return u.areas[index];
-  }
-
-  async toggleUserArea(user: { sub: string }, id: ObjectId): Promise<Area> {
-    const area = await this.getUserArea(user, id);
-    area.active = !area.active;
-    return await this.updateUserArea(user, area);
   }
 }
