c6d217e16525c9b6e1007a709e5be09f0ffa7271
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-10-10 19:03:51 +0200

    fix(AREA-126): rm API propreties from AuthorizationDTO

--- apps/shared/src/dtos/user/authorization.dto.ts
+++ apps/shared/src/dtos/user/authorization.dto.ts
@@ -1,19 +1,7 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty } from "class-validator";
 import { ObjectId } from "mongodb";
 
 export class AuthorizationDto {
-
-    @ApiProperty()
-    @IsEmail()
-    service_Id: ObjectId
-
-    @IsNotEmpty()
-    @ApiProperty()
-    type: string
-
-    @IsNotEmpty()
-    @ApiProperty()
-    data: string
-
+    service_Id: ObjectId;
+    type: string;
+    data: string;
 }
