ff3e366b82d638e95e533c06808b2e2d45f01fbb
Jean-Baptiste Azan <azan.jean-baptiste@epitech.eu>
2024-10-13 21:08:35 +0200

    feat(mobile): oauth2 with google

--- apps/mobileExpo/app/index.tsx
+++ apps/mobileExpo/app/index.tsx
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Dimensions,
   Alert,
+  ActivityIndicator,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Animated, { FadeInDown, FadeInUp } from "react-native-reanimated";
@@ -15,7 +16,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import { makeRedirectUri } from 'expo-auth-session';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -24,29 +24,66 @@ const { width, height } = Dimensions.get("window");
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const API_URL = process.env.EXPO_PUBLIC_API_URL;
   const router = useRouter();
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
-    redirectUri: makeRedirectUri({
-      scheme: 'area',
-      path: 'flowName=GeneralOAuthFlow'
-    }),
-    scopes: ['profile', 'email']
+    androidClientId: "825177499555-dhg7f5e5lfcmj8jk006npd2aupm99jre.apps.googleusercontent.com",
+    redirectUri: "com.jbazan.mobileexpo:/oauth2redirect"
   });
+
+  const handleGoogleAuth = async (idToken: string) => {
+    try {
+      const response = await fetch(`${API_URL}/auth/google/mobile`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({ token: idToken, isMobile: true })
+      });
+      if (!response.ok) {
+        const errorBody = await response.text();
+        console.error("Error response body:", errorBody);
+        throw new Error(`HTTP error! status: ${response.status}, body: ${errorBody}`);
+      }
+
+      const data = await response.json();
+      if (!data.token) {
+        throw new Error('No token received from server');
+      }
+
+      await AsyncStorage.setItem("userToken", data.token);
+      setLoading(false);
+      router.replace("/(auth)/");
+    } catch (error) {
+      console.error('Detailed error during Google authentication:', error);
+      setLoading(false);
+      Alert.alert(
+          "Authentication Error",
+          `Failed to authenticate: ${error.message}. Please try again later.`
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setLoading(true);
+      const { id_token } = response.params;
+      handleGoogleAuth(id_token);
+    } else if (response?.type === "error") {
+      console.error("Google Sign-In error:", response.error);
+      Alert.alert(
+          "Sign-In Error",
+          "An error occurred during Google Sign-In. Please try again."
+      );
+    }
+  }, [response]);
 
   useEffect(() => {
     checkExistingToken();
   }, []);
-
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { authentication } = response;
-      if (!authentication) return;
-      exchangeGoogleTokenForJWT(authentication.accessToken);
-    }
-  }, [response]);
 
   const checkExistingToken = async () => {
     try {
@@ -55,11 +92,12 @@ export default function LoginScreen() {
         router.replace("/(auth)/");
       }
     } catch (error) {
-      console.error("Erreur lors de la vérification du token:", error);
+      console.error("Error checking token:", error);
     }
   };
 
   const handleLogin = async () => {
+    setLoading(true);
     try {
       const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
@@ -74,16 +112,18 @@ export default function LoginScreen() {
       const data = await response.json();
       if (response.ok) {
         await AsyncStorage.setItem("userToken", data.token);
-        router.push('/(auth)/');
+        router.replace("/(auth)/");
       } else {
-        throw new Error("Erreur de connexion");
+        throw new Error("Login error");
       }
     } catch (error) {
-      console.error("Erreur lors de la connexion:", error);
+      console.error("Error during login:", error.message());
       Alert.alert(
-        "Erreur",
-        "Impossible de se connecter. Veuillez vérifier vos identifiants.",
+          "Error",
+          "Unable to login. Please check your credentials.",
       );
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -91,73 +131,66 @@ export default function LoginScreen() {
     promptAsync();
   };
 
-  const exchangeGoogleTokenForJWT = async (googleToken: string) => {
-    try {
-      const response = await fetch(`${API_URL}/auth/google/callback?token=${googleToken}`);
-      const data = await response.json();
-      if (response.ok) {
-        await AsyncStorage.setItem("userToken", data.token);
-        router.push('/(auth)/');
-      } else {
-        throw new Error("Erreur lors de l'échange du token Google");
-      }
-    } catch (error) {
-      console.error("Erreur lors de l'échange du token Google:", error);
-      Alert.alert(
-        "Erreur",
-        "Impossible de se connecter avec Google. Veuillez réessayer.",
-      );
-    }
-  };
-
   const handleSignup = () => {
-    router.push('/Signup');
+    router.push("/Signup");
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <LinearGradient
-        colors={["#4c669f", "#3b5998", "#192f6a"]}
-        style={styles.background}
-      />
-      <Animated.View
-        entering={FadeInUp.duration(1000).springify()}
-        style={styles.titleContainer}
-      >
-        <Text style={styles.title}>Welcome</Text>
-        <Text style={styles.subtitle}>Connect to your account</Text>
-      </Animated.View>
+      <SafeAreaView style={styles.container}>
+        <LinearGradient
+            colors={["#4c669f", "#3b5998", "#192f6a"]}
+            style={styles.background}
+        />
+        <Animated.View
+            entering={FadeInUp.duration(1000).springify()}
+            style={styles.titleContainer}
+        >
+          <Text style={styles.title}>Welcome</Text>
+          <Text style={styles.subtitle}>Connect to your account</Text>
+        </Animated.View>
 
-      <Animated.View
-        entering={FadeInDown.duration(1000).springify()}
-        style={styles.formContainer}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Mot de passe"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Se connecter</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.googleButton} onPress={handleGoogleLogin}>
-          <Text style={styles.buttonText}>Se connecter avec Google</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.signupButton} onPress={handleSignup}>
-          <Text style={styles.signupButtonText}>S'inscrire</Text>
-        </TouchableOpacity>
-      </Animated.View>
-    </SafeAreaView>
+        <Animated.View
+            entering={FadeInDown.duration(1000).springify()}
+            style={styles.formContainer}
+        >
+          <TextInput
+              style={styles.input}
+              placeholder="Email"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+              autoCapitalize="none"
+          />
+          <TextInput
+              style={styles.input}
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+          />
+          <TouchableOpacity
+              style={styles.button}
+              onPress={handleLogin}
+              disabled={loading}
+          >
+            {loading ? (
+                <ActivityIndicator color="white" />
+            ) : (
+                <Text style={styles.buttonText}>Login</Text>
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.googleButton}
+              onPress={handleGoogleLogin}
+              disabled={loading}
+          >
+            <Text style={styles.buttonText}>Login with Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.signupButton} onPress={handleSignup}>
+            <Text style={styles.signupButtonText}>Sign Up</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </SafeAreaView>
   );
 }
 
@@ -184,13 +217,6 @@ const styles = StyleSheet.create({
     color: "white",
     marginBottom: 10,
   },
-  googleButton: {
-    backgroundColor: "#4285F4",
-    borderRadius: 25,
-    padding: 15,
-    alignItems: "center",
-    marginBottom: 10,
-  },
   subtitle: {
     fontSize: 18,
     color: "rgba(255, 255, 255, 0.8)",
@@ -207,6 +233,13 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "#192f6a",
+    borderRadius: 25,
+    padding: 15,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  googleButton: {
+    backgroundColor: "#4285F4",
     borderRadius: 25,
     padding: 15,
     alignItems: "center",
