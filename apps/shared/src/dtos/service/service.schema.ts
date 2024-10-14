6b88f34ee1b812f75d7613de3d00425c786465e4
lemairetheo <114654759+lemairetheo@users.noreply.github.com>
2024-10-14 18:28:35 +0200

    feat(AREA-105): system for display dynamic action and reaction (#59)
    Co-authored-by: Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
    Co-authored-by: Theo Lemaire <theolemaire@MacBook-Air-de-Theo.local>
    Co-authored-by: Jean-Baptiste Azan <azan.jean-baptiste@epitech.eu>

---
+++ apps/shared/src/dtos/service/service.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import { ObjectId } from "mongodb";
+import { ActionInfo } from "./action-info.dto";
+import { ReactionInfo } from "./reaction-info.dto ";
+
+@Schema({versionKey: false})
+export class Service {
+  _id: ObjectId;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ type: [ActionInfo] })
+  actions: ActionInfo[];
+
+  @Prop({ type: [ReactionInfo] })
+  reactions: ReactionInfo[];
+}
