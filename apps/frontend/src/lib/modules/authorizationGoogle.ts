95596c724dbcc02bdf884788f248df3b135316c0
Jean-Baptiste Azan <azan.jean-baptiste@epitech.eu>
2024-10-17 17:39:40 +0200

    fix(frontend): dead code rm

--- apps/frontend/src/lib/modules/authorizationGoogle.ts
+++
@@ -1,7 +0,0 @@
-export function authorizationGoogle() {
-    const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-    const redirectUri = encodeURIComponent(`${window.location.origin}/login/oauth/google`);
-    const scope = encodeURIComponent('email profile https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/tasks');
-    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
-    window.location.href = googleAuthUrl;
-}
