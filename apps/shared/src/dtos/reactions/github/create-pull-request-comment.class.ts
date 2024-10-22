84253d5370382da19793b383f8b7752599505bfc
Léo DUMONT <114572827+leodumont444@users.noreply.github.com>
2024-10-22 22:21:35 +0200

    feat(AREA-180): added create-pull-request-comment reaction (#104)

---
+++ apps/shared/src/dtos/reactions/github/create-pull-request-comment.class.ts
@@ -0,0 +1,23 @@
+import {IsNotEmpty, IsString} from "class-validator";
+import { BaseReactionInfos } from "../reaction-infos.class";
+import { ReactionTypes } from "../reaction-types.enum";
+
+export class CreatePullRequestCommentClass extends BaseReactionInfos {
+    type: ReactionTypes.CREATE_PULL_REQUEST_COMMENT;
+
+    @IsString()
+    @IsNotEmpty()
+    owner: string;
+
+    @IsString()
+    @IsNotEmpty()
+    repository: string;
+
+    @IsString()
+    @IsNotEmpty()
+    pull_request_number: string;
+
+    @IsString()
+    @IsNotEmpty()
+    body: string;
+}
