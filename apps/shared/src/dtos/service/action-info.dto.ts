461603966dd7df16bf70e01ec15519f261b42e0b
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-23 12:41:30 +0200

    feat(services): add action variables to action info in services schema

--- apps/shared/src/dtos/service/action-info.dto.ts
+++ apps/shared/src/dtos/service/action-info.dto.ts
@@ -1,22 +1,21 @@
 import { ApiProperty, } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString, ValidateNested } from "class-validator";
 import { ActionTypes } from "../actions";
 import { ActionParam } from "./action-param.class";
+import { ActionVariable } from "./action-variable.class";
 
 export class ActionInfo {
     @ApiProperty()
-    @IsEmail()
     name: string
 
-    @IsNotEmpty()
     @ApiProperty()
     description: string
 
-    @IsNotEmpty()
     @ApiProperty()
     type: ActionTypes
 
-    @ApiProperty({ type: [Object] })
-    @ValidateNested()
+    @ApiProperty({ type: [ActionParam] })
     params: ActionParam[];
+
+    @ApiProperty({ type: [ActionVariable] })
+    variables: ActionVariable[];
 }
--- apps/shared/src/dtos/service/reaction-info.dto .ts
+++ apps/shared/src/dtos/service/reaction-info.dto .ts
@@ -1,22 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, ValidateNested } from "class-validator";
 import { ReactionTypes } from "../reactions";
 import { ReactionParam } from "./reaction-param.class";
 
 export class ReactionInfo {
     @ApiProperty()
-    @IsEmail()
     name: string
 
-    @IsNotEmpty()
     @ApiProperty()
     description: string
 
-    @IsNotEmpty()
     @ApiProperty()
     type: ReactionTypes
 
     @ApiProperty({ type: [Object] })
-    @ValidateNested()
     params: ReactionParam[];
 }
