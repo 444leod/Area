f24f91fbf7c4e647ee79e1d560ff59a6bfee6870
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-24 18:25:51 +0200

    feat(services.json): add needed authorization for each action / reaction

--- apps/shared/src/dtos/service/reaction-info.dto .ts
+++ apps/shared/src/dtos/service/reaction-info.dto .ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { ReactionTypes } from "../reactions";
 import { ServiceParam } from "./service-param.class";
+import { AuthorizationsTypes } from "../authorizations";
 
 export class ReactionInfo {
   @ApiProperty()
@@ -11,6 +12,9 @@ export class ReactionInfo {
 
   @ApiProperty()
   type: ReactionTypes;
+
+  @ApiProperty({ enum: AuthorizationsTypes })
+  authorization?: AuthorizationsTypes
 
   @ApiProperty({ type: [ServiceParam] })
   params: ServiceParam[];
