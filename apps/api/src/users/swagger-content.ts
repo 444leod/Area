1b7156921c8264373b232b75edea2fd6b56eea9b
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-24 16:02:10 +0200

    feat(UsersController): add swagger content

--- apps/api/src/users/swagger-content.ts
+++ apps/api/src/users/swagger-content.ts
@@ -1,23 +1,30 @@
 import { ApiResponseOptions } from "@nestjs/swagger";
 
 export const UserUnauthorizedOptions: ApiResponseOptions = {
-  description: "Invalid token",
-  status: 401, // Unauthorized
+  description: "Bad user token.",
+  status: 401,
 };
 
-export const AuthorizationOkOptions: ApiResponseOptions = {
+export const ProfileOkOptions: ApiResponseOptions = {
+  description: "A test route that returns a user's informations.",
+  status: 200,
+  example: {
+    _id: "deadbeefdeadbeefdeadbeef",
+    first_name: "John",
+    last_name: "Doe",
+    email: "john.doe@cia.com",
+    password: "SOMEHASHEDPASSWORD",
+    authorizations: [],
+    areas: []
+  }
+};
+
+export const AuthorizationsOkOptions: ApiResponseOptions = {
   description: "List of authorization types for the authenticated user.",
   status: 200,
-  schema: {
-    type: "array",
-    items: {
-      type: "string",
-      example: "Atlassian",
-    },
-  },
-};
-
-export const UserNotFoundOptions: ApiResponseOptions = {
-  description: "User not found.",
-  status: 404,
+  example: [
+    "Atlassian",
+    "Google",
+    "Jira",
+  ],
 };
