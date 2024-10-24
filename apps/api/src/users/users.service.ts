23ca2ee5e2154d4047b2c18dc9fe2e7417da7c87
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-24 17:33:30 +0200

    feat(UserService): made getUserAuths use AuthGuard correctly

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -76,31 +76,9 @@ export class UsersService {
     return await newUser.save();
   }
 
-  async getUserAuthorizations(req: Request): Promise<string[]> {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (!token) {
-      console.error("Token missing or invalid");
-      throw new UnauthorizedException("Invalid token");
-    }
-
-    let tokenPayload: any;
-    try {
-      tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
-    } catch (error) {
-      console.error("Error while verifying the token:", error);
-      throw new UnauthorizedException("Invalid token");
-    }
-
-    const id = tokenPayload.sub;
-    const user: User = await this.userModel.findById(id);
-
-    if (!user) {
-      console.error("User not found");
-      throw new UnauthorizedException("Invalid user");
-    }
-
+  async getUserAuthorizations(_user: AuthentifiedUser): Promise<string[]> {
+    const user: User = await this.userModel.findById(_user.id);
+    if (!user) throw new UnauthorizedException("Invalid user");
     return user.authorizations.map((auth) => auth.type);
   }
 
