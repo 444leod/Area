298fe3d00872936b001573109137b73a4356921d
Theo Lemaire <theolemaire@MacBook-Air-de-Theo.local>
2024-10-30 12:20:34 +0100

    fix(AREA-254):debug disconnect function

--- apps/frontend/src/routes/profile/authorization/+page.svelte
+++ apps/frontend/src/routes/profile/authorization/+page.svelte
@@ -106,26 +106,26 @@
 
 	async function disconnectService(service) {
 		const token = await fetchToken();
-		const type = service.name.toUpperCase();
+		const type = service.name.toUpperCase()
+
 		try {
 			const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/disconnect`, {
 				method: 'DELETE',
 				headers: {
 					'Content-Type': 'application/json',
-					Authorization: `Bearer ${token}`
+					'Authorization': `Bearer ${token}`
 				},
 				body: JSON.stringify({ type: type })
 			});
+
 			if (response.ok) {
-				await response.json();
-				alert('Successfully disconnected');
-				goto('/profile');
-				service.connected = false;
+				alert("Deconnexion reussis")
+				await goto('/profile');
 			} else {
 				throw new Error(`Error during disconnection`);
 			}
 		} catch (error) {
-			throw new Error(`Error during disconnection`);
+			throw new Error(`Error during disconnection ` + error);
 		}
 	}
 
