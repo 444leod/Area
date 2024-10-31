44e2c9034ba3cd4ea663bbfdb1a3fbec80574a72
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-10-31 11:28:51 +0100

    feat(admin-dash): swagger content

--- apps/api/src/admin/admin.controller.ts
+++ apps/api/src/admin/admin.controller.ts
@@ -4,16 +4,19 @@ import { Roles } from "src/roles/roles.decorator";
 import { AuthGuard } from "src/auth/auth.guard";
 import { RolesGuard } from "src/roles/roles.guard";
 import { AdminService } from "./admin.service";
+import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { AdminDashOkResponse } from "./swagger-content";
 
+@ApiTags("Admin")
 @Controller("admin")
 export class AdminController {
+  constructor(private readonly adminService: AdminService) { }
 
-    constructor (private readonly adminService: AdminService) {}
-
-    @Roles(Role.ADMIN)
-    @UseGuards(AuthGuard, RolesGuard)
-    @Get()
-    async getDashboardInformations(): Promise<AdminDashboardInfos> {
-        return await this.adminService.getDashboardInfos();
-    }
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Get()
+  @ApiOkResponse(AdminDashOkResponse)
+  async getDashboardInformations(): Promise<AdminDashboardInfos> {
+    return await this.adminService.getDashboardInfos();
+  }
 }
---
+++ apps/api/src/admin/swagger-content.ts
@@ -0,0 +1,15 @@
+export const AdminDashOkResponse = {
+  example: {
+    user_count: 15,
+    areas_count: 20,
+    active_areas_count: 18,
+    popular_actions: {
+      "EACH_X_SECONDS": 8,
+      "ON_RIOT_GAME_END": 5,
+      "ON_YOUTUBE_VIDEO_POSTED": 7
+    },
+    popular_reactions: {
+      "something": 9999
+    }
+  }
+}
