a755086448b7cc543a8d418545890b577da83206
Jean-Baptiste Azan <azan.jean-baptiste@epitech.eu>
2024-11-01 10:41:22 +0100

    fix(mobile): condition in the dockerfile to skip building

--- apps/mobileExpo/build.sh
+++ apps/mobileExpo/build.sh
@@ -4,7 +4,7 @@ if [ "$1" = "0" ]; then
     npm install
     rm -rf dist/*
     npm install --global eas-cli
-    eas build --local --non-interactive --platform android --profile production --output dist/area.apk
+    eas build --local --non-interactive --platform android --profile production --output dist/client.apk
 else
     echo "Skipping mobile compilation..."
 fi
